56e95063d37c492940f0f52df5792b8e8a079f50
Ilia Barashkov <ibarashkov@list.ru>
2017-05-08 21:25:17 +0300

    Декоративные элементы

---
+++ js/popup.js
@@ -0,0 +1,16 @@
+var open = document.querySelector('.open');
+var popup = document.querySelector('.subscription_dialog');
+var close = popup.querySelector('.close');
+var overlay = document.querySelector('.subscription_dialog_overlay'); 
+
+open.addEventListener('click', function() {
+	popup.classList.remove('closed');
+	overlay.classList.remove('closed');
+	popup.classList.add('summon');
+})
+
+close.addEventListener('click', function() {
+	popup.classList.add('closed');
+	overlay.classList.add('closed');
+	popup.classList.remove('summon');
+})
