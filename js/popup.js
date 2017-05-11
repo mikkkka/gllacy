41043c0e158f0f920102b07df8ddf28bc51c93f8
Ilia Barashkov <ibarashkov@list.ru>
2017-05-12 00:29:24 +0300

    На допуск

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,8 @@ var popup = document.querySelector('.subscription_dialog');
 var close = popup.querySelector('.close');
 var overlay = document.querySelector('.subscription_dialog_overlay'); 
 
-open.addEventListener('click', function() {
+open.addEventListener('click', function(evt) {
+	evt.preventDefault();
 	popup.classList.remove('closed');
 	overlay.classList.remove('closed');
 	popup.classList.add('summon');
